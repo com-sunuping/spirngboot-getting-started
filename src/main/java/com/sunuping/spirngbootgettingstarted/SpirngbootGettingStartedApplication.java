f6a1bd55d6e21bbfa27b5f5b41d478726f06dafa
sunuping <1933305628@qq.com>
2018-11-14 23:58:56 +0800

    springboot getting started

---
+++ src/main/java/com/sunuping/spirngbootgettingstarted/SpirngbootGettingStartedApplication.java
@@ -0,0 +1,12 @@
+package com.sunuping.spirngbootgettingstarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngbootGettingStartedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpirngbootGettingStartedApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sunuping/spirngbootgettingstarted/SpirngbootGettingStartedApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sunuping.spirngbootgettingstarted;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpirngbootGettingStartedApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
